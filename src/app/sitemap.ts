697ad53bfb335f041b028c1092dc7d23dc559e5c
Rishusingh18 <194272463+Rishusingh18@users.noreply.github.com>
2026-03-17 21:02:47 +0530

    fix domain names

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://mkppackers.com';
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://mkppackersmover.com';
 
   return [
     {
